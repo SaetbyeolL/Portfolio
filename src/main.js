82e92509b177ce5106b3737f81656b61eb3070e4
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-03-21 22:25:30 -0400

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-// Apply dark styling to the header when scrolling down
+// Header: Apply dark styling to the header when scrolling down
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
     if(window.scrollY > headerHeight) {
         header.classList.add('header--dark');
     } else {
@@ -11,13 +10,24 @@ document.addEventListener('scroll', () => {
 });
 
 
+//Home: Make the Home section transparent when scrolling down
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', ()=> {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
 
 
 
-
-
-
-
+//Arrow-up: Make the arrow up button transparent when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=> {
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.style.opacity = 1;         
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+});
 
 
 
