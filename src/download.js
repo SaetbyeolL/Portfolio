8d00338f125e40df7f8dde5faba3dcbd62b84f65
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-04-07 19:33:32 -0400

    line cleanup

--- src/download.js
+++ src/download.js
@@ -1,12 +1,9 @@
-'use strict';
-
 document.getElementById("download").addEventListener("click", function() {
     const resumeFilePath = "images/resume/RESUME.pdf";
-
-    // a 태그를 동적으로 생성하여 다운로드 링크 설정
     const link = document.createElement("a");
+
     link.href = resumeFilePath;
-    link.download = "SaetbyeolLim_RESUME.pdf"; // 다운로드되는 파일의 이름 설정
+    link.download = "SaetbyeolLim_RESUME.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
--- src/type.js
+++ src/type.js
@@ -1,6 +1,6 @@
 'use strict';
 
-new TypeIt('.home__title--strong', {speed: 100}) //
+new TypeIt('.home__title--strong', {speed: 100})
 .type(',')
 .break({delay: 500})
 .type('Software Developer')
